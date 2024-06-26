5ddda7d76473d252db86f0f22e519cd04e0a86a4
xtophe38 <christophe.naud@gmail.com>
2024-06-26 16:00:16 +0200

    Set new version #

--- teemip-ip-discovery/module.teemip-ip-discovery.php
+++ teemip-ip-discovery/module.teemip-ip-discovery.php
@@ -1,12 +1,12 @@
 <?php
 /*
- * @copyright   Copyright (C) 2021 TeemIp
+ * @copyright   Copyright (C) 2010-2024 TeemIp
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'teemip-ip-discovery/3.1.2-dev',
+	'teemip-ip-discovery/3.2.0',
 	array(
 		// Identification
 		//
